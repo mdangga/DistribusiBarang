71d0a6098c286734035f5a8cfe1d2508cdb9006b
mdangga <anggamurdika2005@gmail.com>
2025-05-15 07:53:52 +0800

    update component

--- resources/views/components/button-sidebar.blade.php
+++ resources/views/components/button-sidebar.blade.php
@@ -1,14 +1,10 @@
 @props(['route', 'icon'])
 
 <li>
-    <a href="{{ $route }}" {{ $attributes->merge(['class' => 'flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100']) }}>
-        <svg 
-            class="w-5 h-5 text-gray-500 transition duration-75" 
-            aria-hidden="true" 
-            xmlns="http://www.w3.org/2000/svg" 
-            fill="currentColor" 
-            viewBox="0 0 24 24"
-        >
+    <a href="{{ $route }}" class="flex items-center p-2 text-gray-700 rounded-lg  hover:bg-gray-100 group">
+        <svg xmlns="http://www.w3.org/2000/svg"
+            class="w-5 h-5 text-gray-700 transition duration-75  group-hover:text-gray-900" width="576" height="512"
+            viewBox="0 0 576 512">
             {!! $icon !!}
         </svg>
         <span class="ms-3">{{ $slot }}</span>
